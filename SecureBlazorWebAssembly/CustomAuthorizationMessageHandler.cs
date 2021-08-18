646cbcf69b700c34f22fc7eb95e93c58b02f21e9
EnOSeChi <slawekhus@gmail.com>
2021-08-18 17:09:00 +0200

    custom auth message handler

---
+++ SecureBlazorWebAssembly/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecureBlazorWebAssembly
+{
+    public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+    {
+        public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+            NavigationManager navigationManager)
+            : base(provider, navigationManager)
+        {
+            ConfigureHandler(
+                authorizedUrls: new[] { "http://localhost:4000" },
+                scopes: new[] { "profile" });
+        }
+    }
+}
--- SecureBlazorWebAssembly/Program.cs
+++ SecureBlazorWebAssembly/Program.cs
@@ -15,11 +15,13 @@ namespace SecureBlazorWebAssembly
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddHttpClient("WebAPI",
-                    client => client.BaseAddress = new Uri("https://google.com"))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+                    client => client.BaseAddress = new Uri("http://localhost:4000"))
+                .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
 
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
                 .CreateClient("WebAPI"));
