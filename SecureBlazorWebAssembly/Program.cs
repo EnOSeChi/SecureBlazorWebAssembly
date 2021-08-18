a1114b2b8aa24ffd732f382b5bdb2696f6ce8c04
EnOSeChi <slawekhus@gmail.com>
2021-08-18 16:20:45 +0200

    inject http client factory

--- SecureBlazorWebAssembly/Program.cs
+++ SecureBlazorWebAssembly/Program.cs
@@ -1,11 +1,9 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SecureBlazorWebAssembly
@@ -18,6 +16,13 @@ namespace SecureBlazorWebAssembly
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddHttpClient("WebAPI",
+                    client => client.BaseAddress = new Uri("https://google.com"))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+                .CreateClient("WebAPI"));
 
             builder.Services.AddOidcAuthentication(options =>
             {
